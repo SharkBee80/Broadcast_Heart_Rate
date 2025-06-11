49785f7a2e468bd2b6c1af411eda5d86632a88ec
SharkBee80 <115618339+SharkBee80@users.noreply.github.com>
2025-06-12 00:38:37 +0800

    refactor(main): 重构主程序和 WebUI_api 类
    -调整 main.py 中的程序结构，移除不必要的初始化步骤- 重构 WebUI_api 类的构造函数和初始化逻辑
    -移除 WebUI_api 中未使用的冗余方法
    - 优化窗口创建和 API 初始化的顺序

--- main.py
+++ main.py
@@ -3,22 +3,19 @@ import webview
 from src.backend.mainform import WebUI_api
 
 if __name__ == '__main__':
-    api = WebUI_api.WebUI_api()
-
     webview.logger.disabled = True
     webview.settings['OPEN_DEVTOOLS_IN_DEBUG'] = False
     # 创建窗口并加载 HTML 页面
     window = webview.create_window(
         '蓝牙心率监测',
         'http://127.0.0.1:25432/main',
-        # js_api=api,
         width=816,  # 800 + 16
         height=642,  # 600 + 28 +16
         resizable=False
     )
 
-    window.events.closed += api.on_closed
+    api = WebUI_api.WebUI_api(window)
 
-    api.init(window)
+    window.events.closed += api.on_closed
 
     webview.start(debug=True, gui='edgechromium')
--- src/backend/mainform/WebUI_api.py
+++ src/backend/mainform/WebUI_api.py
@@ -12,33 +12,14 @@ float_window = Float_window.FloatWindow()
 
 class WebUI_api:
     # 提供给前端调用的方法
-    def __init__(self):
-        self._fetch_thread = None
-        self._fetch_active = False
-        self._fetch_event = threading.Event()  # 使用线程事件替代布尔标志
-        self.window = None
-        pass
-
-    def init(self, window):
+    def __init__(self,  window):
         self.window = window
-        ble.init(window)
-        web.init(window)
-        float_window.init(window)
 
-        self.window.expose(self.refresh_devices, self.set_device, self.connect_device, self.disconnect_device,
-                           self.onload_init)
+        ble.init(self.window)
+        web.init(self.window)
+        float_window.init(self.window)
 
-    def refresh_devices(self):
-        ble.refresh_devices()
-
-    def set_device(self, device):
-        ble.set_device(device)
-
-    def connect_device(self):
-        ble.connect_device()
-
-    def disconnect_device(self):
-        ble.disconnect_device()
+        self.window.expose(self.onload_init)
 
     def onload_init(self):
         web.set_()
